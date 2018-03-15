d79edbfaf2881980ff1f2de17ae848c628aa02fb
HubadevKP <37345892+HubadevKP@users.noreply.github.com>
2018-03-15 13:45:57 +0000

    New branch to 14 line
    # => More branches in line 14
    # => New variable "umb" define by new input

--- weathercheckerKP/weathercheckerKP.py
+++ weathercheckerKP/weathercheckerKP.py
@@ -1,5 +1,8 @@
-# WeathercheckerKP ver.0.00.0.1
-# print, input, if, assigments, bool, com, comparisons
+# WeathercheckerKP ver.0.00.0.2
+# Changes:
+# => More branches in line 14
+# => New variable "umb" define by new input
+# print, input, if, assigments, bool, comparisons, basic branches.
 # Any input = True // No input == False
 # Code inspired by https://automatetheboringstuff.com/chapter2/
 # Code under developed :)
@@ -12,6 +15,13 @@ y = input(bool)
 wind = y
 if rain and wind:
     print('You need a jacket and umbrella')
+    print('Do you have umbrella?')
+    z = input(bool)
+    umb = z
+    if not umb:
+        print('Stay home')
+    else:
+        print('Is raining man!!! Halleluya')
 if rain != wind:
     print('Better to take a jacket')
 if not rain and not wind:
