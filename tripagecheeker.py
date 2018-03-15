6dd77030c80dd98fa226579c1645206f147f48cb
HubadevKP <37345892+HubadevKP@users.noreply.github.com>
2018-03-15 11:16:30 +0000

    Are you to old for trip?
    Data changes, basic statement, inputs and cool error sim.

---
+++ tripagecheeker.py
@@ -0,0 +1,19 @@
+# My own interpretation of code what i found on https://automatetheboringstuff.com/chapter1/.
+# I change that code to pretending that he got a error(based on your inpuy) and if you to old, saddly, you not allow to trip
+# + some data type changes.
+
+
+print('What is your name?')
+name = input()
+print('Error: &&&&##' +str(name[1:]) + '33##11' +str(name)[0:-3] + '!QQ@@#') #error simulation :D 
+print('Status: error fixed') # autofixed simulation xD
+print('...')
+print('...')
+print('It is good to meet you, ' + name)
+print('Nice error mate? BTW length of your name is:')
+print(len(name))
+print('What is your age?')
+myAge = input(int)
+if int(myAge) >= 20:
+    print('To old for school trip... anyway:')
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
