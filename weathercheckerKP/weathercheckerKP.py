19a5e14e4b908434b8aa0f7b1a1ab646e2fd88e9
HubadevKP <37345892+HubadevKP@users.noreply.github.com>
2018-03-15 22:18:19 +0000

    Replace print with while
    34 line:
    count = 0
    while count < 5:
        print('.')
        count += 1

--- weathercheckerKP/weathercheckerKP.py
+++ weathercheckerKP/weathercheckerKP.py
@@ -1,11 +1,16 @@
-# WeathercheckerKP ver.0.00.0.2
-# Changes:
+# WeathercheckerKP ver.0.00.0.3
+# Changlog: 0.00.0.1 -> 0.00.0.2:
 # => More branches in line 14
 # => New variable "umb" define by new input
+# Changlog: 0.00.0.2 -> 0.00.0.3:
+# => Replace 3 lies of prit to while
 # print, input, if, else, assigments, bool, comparisons, basic branches
 # Any input = True // No input == False
 # Code inspired by https://automatetheboringstuff.com/chapter2/
 # Code under developed :)
+# ToDo: 
+# => Script to auto chagelog
+# => Input from real source
 
 print('It is raining?:')
 x = input(bool)
@@ -26,7 +31,8 @@ if rain != wind:
     print('Better to take a jacket')
 if not rain and not wind:
     print('Anyway you go allways take a weather with you')
-print(".")
-print(".")
-print(".")
+count = 0
+while count < 5:
+    print('.')
+    count += 1
 print('WeathercheckerKP ver.0.00.0.1 by @Hubadev')
